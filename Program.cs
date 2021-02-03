3ba6501fce34e42e5d2c1aee951e482017e1c4bc
edyzbelly <77354889+edyzbelly@users.noreply.github.com>
2021-02-03 13:49:12 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your name: ");
+
+            string name = Console.ReadLine();
+
+            Display_Name(name);
+        }
+        
+        private static void Display_Name(string name)
+        {
+            Console.WriteLine("Hello " + name + "!");
+        }    
+    }
+}
